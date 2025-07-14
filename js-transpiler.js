b45e37409c6ba845ecb362f9ccca26ab2920e3a0
Kravataf <159631603+Kravataf@users.noreply.github.com>
2025-07-14 22:13:41 +0200

    tokens[0] switchcase wip

--- js-transpiler.js
+++ js-transpiler.js
@@ -31,5 +31,36 @@ async function readFile(address) {
   for (let line of lines) {
     // tokens == words
     const tokens = line.trim().split(/\s+/);
+
+    switch (tokens[0]) {
+      // the main keywords that are used to define stuff
+      case 'var': // <-- [2] is the type, ( num, str, bool, etc. )
+        break;
+      case 'func': // <-- more complicated do later ( func + the 2 below )
+        break;
+      case 'return':
+        break;
+      case 'call':
+        break;
+      // do these later
+      case 'if':
+        break;
+      case 'else':
+        break;
+      case 'for':
+        break;
+      // math operations
+      case '+':
+        break;
+      case '-':
+        break;
+      case '*':
+        break;
+      case '/':
+        break;
+      // misc
+      default:
+        break;
+    }
   }
 }
